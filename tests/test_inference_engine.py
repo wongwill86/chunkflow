25f3e241c354d8e1fb516226050dd102dba23ac6
wongwill86-mv-2 <wongwill86@gmail.com>
2018-05-11 07:56:06 -0400

    the multithread

--- tests/test_inference_engine.py
+++ tests/test_inference_engine.py
@@ -3,6 +3,9 @@ import unittest
 
 from rx import Observable
 from rx.concurrency import ThreadPoolScheduler
+from rx.core.blockingobservable import BlockingObservable
+from rx import config
+from rx.internal import extensionmethod
 
 from chunkflow import iterators
 from threading import current_thread
@@ -10,213 +13,32 @@ from threading import current_thread
 # from rx.subjects import Subject
 
 
-def run_inference(index):
-    print('\t(%s) running inference at index %s' % (current_thread().name, index,))
-
-def blend(index):
-    print('(%s) ---- blending at %s' % (current_thread().name, index,))
-
-def create_blend(index):
-    return lambda x: blend(index)
-
-def upload(index):
-    print('(%s) uploading %s' % (current_thread().name, index,))
-
-def clear(index):
-    print('(%s) clearing %s' % (current_thread().name, index,))
-
-def inference_done(index):
-    return index in done
-
-def check_blend(neighbors, index):
-    neighbors = [neighbor for neighbor in neighbors if inside_bounds(neighbor)]
-    # Observable.from_list(neighbors).all(inference_done).
-    if any(neighbor not in done for neighbor in neighbors):
-        print('\t\tNot running blend at %s neighbors: %s done: %s' % (index, neighbors, done))
-        return Observable.from_list(neighbors)
-    else:
-        # blend(index)
-        print('\t\t****** Running blend at %s neighbors: %s ' % (index, neighbors,))
-        blended.append(index)
-        return Observable.empty()
-
-def check_inference(x):
-    if x not in done:
-        RuntimeError
-        print('\trunning inference at index %s' % (x,))
-        done.add(x)
-    else:
-        print('\tAlready ran inference %s' % (x,))
-    return x
-
-def recurse(index):
-    # print('recursing at %s ' % (index, ))
-    neighbor_stream = (
-        Observable.just(index)
-        .do_action(lambda x: print('recurse observing: %s' % (x,)))
-        .map(iterators.get_all_neighbors)
-        # .map(lambda x: [neighbor for neighbor in x if inside_bounds(neighbor)])
-        .map(lambda x: check_blend(x, index))
-        .flat_map(lambda x: x)
-        # .filter(inside_bounds)
-    )
-
-    count[0] = count[0] + 1
-    if count[0] > 63:
-        print('BLAHALSDFJKLSD:FJLKSFDJKLSFKJLSFJKLJKLF')
-        return Observable.empty()
-    return neighbor_stream.flat_map(recurse)
-
-optimal_thread_count = multiprocessing.cpu_count()
-pool_scheduler = ThreadPoolScheduler(optimal_thread_count)
-parents = set()
-
-done = set()
-blended = []
-count = [0]
 
 class InferenceEngineTest(unittest.TestCase):
-    # def test_blah(self):
-    #     parents.clear()
-    #     start = (0, 0)
-    #     parents.add(start)
-    #     blah_stream = Observable.just(start).flat_map(recurse)#.publish()
-    #     blah_stream.subscribe(lambda x: print('sub %s' % (x,)))
-    #     # blah_stream.connect()
-    #     print(done)
-    #     print(len(done))
-    #     print(set(done))
-    #     print(len(set(done)))
-    #     print(blended)
-    #     print(len(blended))
-    #     print(set(blended))
-    #     print(len(set(blended)))
-    #     import time
-    #     # assert False
-
-    # def test_blah2(self):
-    #     start = (0, 0)
-    #     src = Observable.just(start)
-
-    #     def get_inference_stream():
-    #         inference_stream = Subject()
-    #         # inference_stream = (
-    #         (
-    #             inference_stream
-    #             # Observable.just(index)
-    #             # .filter(inside_bounds)
-    #             .filter(lambda x: x not in done)
-    #             .do_action(run_inference)
-    #             .do_action(lambda x: print('adding to done %s' % (x,)) or done.add(x))
-    #             .do_action(lambda x: blend_stream.on_next(x))
-    #             .flat_map(get_all_neighbors)
-    #             .map(lambda x: inference_stream.on_next(x))
-    #             .subscribe(print)
-    #         )
-    #         # inference_stream.subscribe(print)
-    #         return inference_stream
-
-    #     blend_stream = Subject()
-    #     (
-    #         blend_stream
-    #         .map(get_all_neighbors)
-    #         .do_action(lambda x: print('here is neigh %s' % (x,)))
-    #         .flat_map(lambda x:
-    #                   Observable.if_then(lambda: not any(outside_bounds(neighbor) for neighbor in x),
-    #                                      Observable.from_list(x),
-    #                                      Observable.empty())
-    #              )
-    #         .subscribe(lambda x: get_inference_stream().on_next(x))
-    #         # .flat_map(lambda x:
-    #         #           Observable.if_then(lambda: any(outside_bounds(neighbor) for neighbor in x),
-    #         #                              Observable.from_list(x),
-    #         #                              Observable.empty)
-    #         # )
-    #         # .filter(inside_bounds)
-    #         # .do_action(lambda x: Observable.just('sadfkjsadfkld;jsl;')
-    #         # .subscribe(print)
-    #         # .subscribe(lambda x: print('hi'))
-    #     )
 
 
-    #     # blend_stream.on_next((0,0))
+    @extensionmethod(BlockingObservable)
+    def blocking_subscribe(source, on_next = None, on_error = None, on_completed = None):
+        latch = config['concurrency'].Event()
 
-    #     get_inference_stream().on_next(start)
-    #     # src.subscribe(get_inference_stream)
-    #     # assert False
+        def onNext(src):
+            if on_next:
+                on_next(src)
 
-    # def test_blah3(self):
-    #     start = (0, 0)
+        def onError(src):
+            if on_error:
+                on_error(src)
+            latch.set()
 
-    #     src = Observable.just(start)
-    #     blend_stream = Subject()
-    #     (
-    #         blend_stream
-    #         .do_action(lambda x: print('...blend_stream called with %s' % (x,)))
-    #         .map(lambda x: ([neighbor for neighbor in get_all_neighbors(x) if inside_bounds(neighbor)], x))
-    #         .do_action(lambda x: print('here is neigh %s' % (x,)))
-    #         .flat_map(lambda x:
-    #                   Observable.from_list(x[0]) if any(neighbor not in done for neighbor in x[0]) else
-    #                   blend(x[1]) or Observable.empty()
-    #                   )
-    #         .filter(inside_bounds)
-    #         .filter(lambda x: x not in done)
-    #         .subscribe(lambda x: inference_stream.on_next(x))
-    #     )
-    #     inference_stream = Subject()
-    #     (
-    #         inference_stream
-    #         .do_action(lambda x: print('inf got %s' % (x,)))
-    #         .do_action(run_inference)
-    #         .do_action(lambda x: done.add(x))
-    #         .subscribe(lambda x: blend_stream.on_next(x))
-    #     )
+        def onCompleted():
+            if on_completed:
+                on_completed()
+            latch.set()
 
-    #     print('runnin')
-    #     done.clear()
-    #     print(done)
+        disposable = source.subscribe(onNext, onError, onCompleted)
+        latch.wait()
 
-    #     inference_stream.on_next(start)
-
-
-    # def test_bfs(self):
-    #     start = (0, 0)
-    #     enqueue = Subject()
-    #     dequeue = Subject()
-    #     visited = set()
-    #     done.clear()
-    #     inference_stream = Subject()
-
-
-    #     (
-    #         enqueue
-    #         .do_action(inference_stream)
-    #         .do_action(lambda x: visited.add(x))
-    #         .do_action(lambda x: print('got to enqueue %s visited: %s' % (x, visited)))
-    #         .flat_map(get_all_neighbors)
-    #         .filter(lambda x: x not in visited)
-    #         .filter(inside_bounds)
-    #         .subscribe(enqueue)
-    #         # .subscribe(lambda x: enqueue.on_next(x))
-    #         # .subscribe(inference_stream)
-    #     )
-
-    #     inference_stream.do_action(run_inference).map(
-    #         lambda x: (
-    #             x,
-    #             get_all_neighbors(x)
-    #             # Observable.from_list(get_all_neighbors(x)).filter(lambda x: x not in visited).filter(inside_bounds)
-    #             )
-    #     ).filter(
-    #         # lambda x: x[1].all(lambda y: False and print('hello %s, visited is %s' % (x, visited)) and y in visited)
-    #         lambda x: all([neighbor in visited for neighbor in x[1] if inside_bounds(neighbor)])
-    #     ).subscribe(lambda x: blend(x[0]))
-
-    #     # dequeue.subscribe(print)
-
-
-    #     # src = Observable.just(start).subscribe(enqueue)
-    #     enqueue.on_next(start)
+        return disposable
 
     def test_with_iter(self):
         start = (0, 0)
@@ -262,8 +84,21 @@ class InferenceEngineTest(unittest.TestCase):
         # assert False
 
     def test_ugh(self):
+        optimal_thread_count = multiprocessing.cpu_count()
         scheduler = ThreadPoolScheduler(optimal_thread_count)
+        print('optimal thread count %s' % optimal_thread_count)
 
+        from rx import config
+        self.latch = config['concurrency'].Event()
+        # def on_completed():
+        #     print('Completed...')
+        #     self.latch.set()
+        # Observable.from_(range(5)) \
+        #     .zip(Observable.interval(1000), lambda x,y: x) \
+        #     .subscribe_on(scheduler) \
+        #     .blocking_subscribe(print, print, on_completed)
+        # self.latch.wait()
+        # Observable.range(1, 20) \
         # Observable.range(0, 100)\
         #     .select_many(lambda x: Observable.start(lambda: x, scheduler=scheduler)) \
         #     .map(lambda i: i * 100) \
@@ -272,19 +107,20 @@ class InferenceEngineTest(unittest.TestCase):
         #     .subscribe(on_next=lambda i: print("PROCESS 3: {0} {1}".format(current_thread().name, i)),
         #                on_error=lambda e: print(e))
         (
-            Observable.range(1, 10)
-                .select_many(lambda i: Observable.start(lambda: i, scheduler=scheduler))
-
-                # .observe_on(Scheduler.event_loop)
-                # .observe_on(scheduler)
-                .do_action(intense_calculation)
-
-                .subscribe(
-                    on_next=lambda x: printthread("on_next: {}".format(x)),
-                    on_completed=lambda: printthread("on_completed"),
-                    on_error=lambda err: printthread("on_error: {}".format(err)))
+            Observable.from_(range(1, 10, 5))
+            .do_action(intense_calculation2)
+            # .select_many(lambda i: Observable.start(lambda: i, scheduler=scheduler))
+            .observe_on(scheduler)
+            .flat_map(lambda x: Observable.from_(range(x, x + 5), scheduler=scheduler))
+            # .observe_on(scheduler)
+            .do_action(intense_calculation)
+            .do_action(intense_calculation3)
+            .to_blocking()
+            .blocking_subscribe(
+                on_next=lambda x: printthread("on_next: {}".format(x)),
+                on_completed=lambda: printthread("on_completed"),
+                on_error=lambda err: printthread("on_error: {}".format(err)))
         )
-        # Observable.range(1, 20) \
         #     .map(lambda s: intense_calculation(s)) \
         #     .subscribe_on(pool_scheduler) \
         #     .subscribe(on_next=lambda i: print("PROCESS 2: {0} {1}".format(current_thread().name, i)),
@@ -299,7 +135,7 @@ class InferenceEngineTest(unittest.TestCase):
         #             on_error=lambda e: print(e))
 
         printthread("\nAll done")
-        scheduler.executor.shutdown(wait=True)
+        # scheduler.executor.shutdown(wait=True)
         # time.sleep(4)
         # pool_scheduler.executor.shutdown()
         assert False
@@ -311,8 +147,16 @@ from rx.core import Scheduler
 def printthread(val):
     print("{}, \tthread: {}".format(val, current_thread().name))
 
-def intense_calculation(value):
-    printthread("calc {}".format(value))
-    time.sleep(.5)
+def intense_calculation2(value):
+    printthread("pre caclc {}".format(value))
+    # time.sleep(random.random() * 2)
     return value
 
+def intense_calculation(value):
+    printthread("\t\tcalc {}".format(value))
+    time.sleep(random.random() * 1)
+    return value
+
+def intense_calculation3(value):
+    printthread("\t\t\tpost calce {}".format(value))
+    time.sleep(random.random() * 1)
