93c3dcba2395072d3c71495ae980d18a30a76868
wongwill86 <wongwill86@gmail.com>
2018-06-01 18:07:44 -0400

    huh corder?

--- src/chunkflow/cloudvolume_datasource.py
+++ src/chunkflow/cloudvolume_datasource.py
@@ -3,27 +3,46 @@ from chunkflow.global_offset_array import GlobalOffsetArray
 from chunkflow.datasource_manager import DatasourceRepository
 from cloudvolume import CloudVolume
 
-class CloudVolumeWrapper(CloudVolume):
+class CloudVolumeCZYX(CloudVolume):
+    """
+    Cloudvolume assumes XYZC Fortran order.  This class hijacks cloud volume indexing to use CZYX C order indexing
+    instead. All other usages of indices such as in the constructor are STILL in ZYXC fortran order!!!!
+    """
+
     def __getitem__(self, slices):
-        print(slices)
         offset = [s.start for s in slices if isinstance(s, slice)]
-        print(offset)
-        item = super().__getitem__(slices);
+
+        # reverse to Fortran xyzc order
+        slices = slices[::-1]
+
+        item = super().__getitem__(slices)
         if item.flags['F_CONTIGUOUS']:
             item = item.transpose()
             item = np.ascontiguousarray(item)
 
-        GlobalOffsetArray(super().__getitem__(slices), global_offset=offset)
+        return GlobalOffsetArray(item, global_offset=offset)
+
+
+    def __setitem__(self, slices, item):
+        slices = slices[::-1]
+        if item.flags['C_CONTIGUOUS']:
+            item = item.transpose()
+            item = np.asfortranarray(item)
+        super().__setitem__(slices, item)
 
 
 class CloudVolumeDatasource(DatasourceRepository):
     def __init__(self, input_cloudvolume, *args, **kwargs):
         # input_datasource = CloudVolumeWrapper(input_cloudvolume)
-        super().__init__(input_datasource, *args, **kwargs)
+        if not isinstance(input_cloudvolume, CloudVolumeCZYX):
+            raise NotImplementedError('Must use %s class cloudvolume to ensure correct indexing' %
+                                      CloudVolumeCZYX.__class__)
+        super().__init__(input_cloudvolume, *args, **kwargs)
 
     def create(self, mod_index, *args, **kwargs):
-        offset = self.input_datasource.global_offset
-        shape = self.input_datasource.shape
+        pass
+        # offset = self.input_datasource.global_offset
+        # shape = self.input_datasource.shape
 
-        return GlobalOffsetArray(np.zeros(shape), global_offset=offset)
+        # return GlobalOffsetArray(np.zeros(shape), global_offset=offset)
 
--- tests/test_cloudvolume_datasource.py
+++ tests/test_cloudvolume_datasource.py
@@ -1,17 +1,57 @@
 import unittest
-import cloudvolume
-# from cloudvolume import CloudVolume
-from chunkflow.cloudvolume_datasource import CloudVolumeWrapper
+import pytest
+import numpy as np
+from cloudvolume import CloudVolume
+from chunkflow.cloudvolume_datasource import CloudVolumeCZYX
 from chunkflow.cloudvolume_datasource import CloudVolumeDatasource
+
+
+class CloudVolumeCZYXTest(unittest.TestCase):
+
+    @pytest.fixture(autouse=True)
+    def test_get(self, tmpdir_factory):
+        folder = 'file://' + tmpdir_factory.mktemp('test_get').strpath
+        data_shape_fortran = [4, 4, 1]
+        slices_fortran = tuple(slice(0, d) for d in data_shape_fortran)
+        slices_c = slices_fortran[::-1]
+
+        info = CloudVolume.create_new_info(
+            num_channels=1,
+            layer_type='image',
+            data_type='uint8',
+            encoding='raw',
+            chunk_size=data_shape_fortran,
+            volume_size=data_shape_fortran,
+            resolution=[1, 1, 1],
+            voxel_offset=[0, 0, 0]
+        )
+        cv_fortran = CloudVolume(folder, info=info, non_aligned_writes=True, fill_missing=True, compress=False)
+        cv_fortran.commit_info()
+
+        # F ordered czyx
+        data_fortran = np.arange(0, np.product(data_shape_fortran), dtype=np.uint8).reshape(
+            data_shape_fortran, order='F')
+        cv_fortran[slices_fortran] = data_fortran
+        expected = cv_fortran[slices_fortran]
+        print(expected.flatten())
+
+        cv_c = CloudVolumeCZYX(folder, info=info, non_aligned_writes=True, fill_missing=True, compress=False)
+        actual = cv_c[slices_c].flatten()
+        import binascii
+        print(binascii.b2a_hex(actual.tobytes()))
+        print(actual)
+        print(folder)
+
+        assert False
+
 
 class CloudVolumeDatasourceTest(unittest.TestCase):
 
     def test_nothing(self):
-        input_cloudvolume = CloudVolumeWrapper('gs://wwong/sub_pinky40_v11/image/', cache=True)
+        input_cloudvolume = CloudVolumeCZYX('gs://wwong/sub_pinky40_v11/image/', cache=True)
         # cloudvolume_datasource = CloudVolumeDatasource(input_cloudvolume)
-        data = input_cloudvolume[10240:10240+64, 40960:40960+64,0:16]
+        data = input_cloudvolume[0:16, 40960:40960+64, 10240:10240+64]
         print(data.shape)
         print(data.flags)
-
 
         assert False
