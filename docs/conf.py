9e64d8063291e9024545a36931445a86b8126a01
wongwill86-mv-2 <wongwill86@gmail.com>
2018-06-19 09:46:06 -0400

    adding auto api docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'autoapi.extension',
 ]
 if os.getenv('SPELLCHECK'):
     extensions += 'sphinxcontrib.spelling',
@@ -52,3 +53,6 @@ html_short_title = '%s-%s' % (project, version)
 napoleon_use_ivar = True
 napoleon_use_rtype = False
 napoleon_use_param = False
+
+autoapi_type = 'python'
+autoapi_dirs = ['../src/chunkflow']
