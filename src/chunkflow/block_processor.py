3f2acd206c2104f5e9beef4e36643eff5d137202
wongwill86-mv-2 <wongwill86@gmail.com>
2018-10-11 20:07:58 -0400

    Try different iterator

--- src/chunkflow/block_processor.py
+++ src/chunkflow/block_processor.py
@@ -2,16 +2,66 @@ import traceback
 from datetime import datetime
 from functools import reduce
 from threading import current_thread
+from chunkblocks.iterators import UnitIterator
+from chunkflow.streams import blocking_subscribe
 
 from rx import Observable
 
 
-class BlockProcessor:
+class ReadyNeighborIterator(UnitIterator):
     def __init__(self, block):
+        self.block = block
+        self.num_chunks = block.num_chunks
+
+    def get(self, start=None, dimensions=None):
+        if start is None:
+            start = (0,) * len(self.num_chunks)
+        queue = []
+        queue.append(start)
+
+        queued = set()
+        completed = set()
+        def mark_done(marked_index):
+            completed.add(marked_index)
+            candidate_list = []
+            for neighbor_index in self.get_all_neighbors(marked_index, self.num_chunks):
+                candidates = []
+                # schedule neighbors of neighbors if necessary
+                for neighbor_neighbor_index in self.get_all_neighbors(neighbor_index, self.num_chunks):
+                    if neighbor_neighbor_index not in completed:
+                        candidates.append(neighbor_neighbor_index)
+                candidate_list.append(candidates)
+
+            candidate_list.sort(key=len)
+            to_queue = [
+                candidate for candidates in candidate_list for candidate in candidates if candidate not in queued
+            ]
+
+            queue.extend(to_queue)
+            for item in to_queue:
+                queued.add(item)
+
+        mark_done(start)
+
+        mark = None
+        while len(queue) > 0:
+            if mark is None:
+                mark = yield self.block.unit_index_to_chunk(queue.pop())
+            else:
+                mark_done(mark.unit_index)
+                mark = yield None
+
+
+class BlockProcessor:
+    def __init__(self, block, iterator=None, on_next=None, on_error=None, on_completed=None):
         self.block = block
         self.num_chunks = reduce(lambda x, y: x * y, block.num_chunks)
         self.completed = 0
         self.error = None
+        self.on_next = on_next
+        self.on_error = on_error
+        self.on_completed = on_completed
+        self.iterator = iterator
 
     def process(self, processing_stream, start_slice=None):
         print('Num_chunks %s' % (self.block.num_chunks,))
@@ -20,20 +70,34 @@ class BlockProcessor:
         else:
             start = tuple([0] * len(self.block.bounds))
 
+        if self.iterator is None:
+            iterator = self.block.chunk_iterator(start)
+
         (
-            Observable.from_(self.block.chunk_iterator(start))
+            Observable.from_(iterator)
+            .do_action(lambda ugh: print('ugh is ', ugh))
             .flat_map(processing_stream)
             .to_blocking()
-            .blocking_subscribe(self.print_done, on_error=self.on_error)
+            .blocking_subscribe(self._on_next, on_error=self._on_error, on_completed=self._on_completed)
         )
 
-    def on_error(self, error):
+    def _on_completed(self):
+        print('Finished processing', self.num_chunks)
+        if self.on_completed is not None:
+            self.on_completed()
+
+
+    def _on_error(self, error):
         print('\n\n\n\n************************************error************************************\n\n')
         self.error = error
         traceback.print_exception(None, error, error.__traceback__)
+        if self.on_error is not None:
+            self.on_error(error)
         raise error
 
-    def print_done(self, chunk, data=None):
+    def _on_next(self, chunk, data=None):
         self.completed += 1
         print('****** %s--%s %s. %s of %s done ' % (datetime.now(), current_thread().name, chunk.unit_index,
                                                     self.completed, self.num_chunks))
+        if self.on_next is not None:
+            self.on_next(chunk)
--- tests/test_block_processor.py
+++ tests/test_block_processor.py
@@ -1,13 +1,15 @@
 import pytest
 from chunkblocks.models import Block
 from rx import Observable
+from functools import reduce
 
-from chunkflow.block_processor import BlockProcessor
+from chunkflow.block_processor import BlockProcessor, ReadyNeighborIterator
 
 
 class TestBlockProcessor:
 
-    def test_process_single_channel_2x2(self):
+    def test_error_throw(self):
+        return
         bounds = (slice(0, 5), slice(0, 5))
         chunk_shape = (3, 3)
         overlap = (1, 1)
@@ -28,3 +30,57 @@ class TestBlockProcessor:
             block_processor.process(task_stream)
 
         assert block_processor.error is not None
+
+    def test_ready_iterator_integration(self):
+        bounds = (slice(0, 9), slice(0, 9))
+        chunk_shape = (3, 3)
+        overlap = (1, 1)
+
+        block = Block(bounds=bounds, chunk_shape=chunk_shape, overlap=overlap)
+
+        block_processor = BlockProcessor(block, on_next=lambda chunk: accumulate_next(chunk))
+        block_processor.process(lambda chunk: Observable.just(chunk))
+
+        assert reduce(lambda x, y: x * y, block.num_chunks) == len(completed)
+
+    def test_ready_iterator_integration(self):
+        bounds = (slice(0, 9), slice(0, 9))
+        chunk_shape = (3, 3)
+        overlap = (1, 1)
+
+        block = Block(bounds=bounds, chunk_shape=chunk_shape, overlap=overlap)
+
+        done_iterator = ReadyNeighborIterator(block).get()
+        done_iterator.send(None)
+        completed = set()
+        def accumulate_next(chunk):
+            completed.add(chunk)
+
+        block_processor = BlockProcessor(block,
+                                         on_next=lambda chunk: accumulate_next(chunk) or done_iterator.send(chunk))
+        block_processor.process(lambda chunk: Observable.just(chunk))
+
+        assert reduce(lambda x, y: x * y, block.num_chunks) == len(completed)
+
+
+class TestReadyNeighborIterator:
+    def test_done_iterator(self):
+        bounds = (slice(0, 9), slice(0, 9))
+        chunk_shape = (3, 3)
+        overlap = (1, 1)
+
+        block = Block(bounds=bounds, chunk_shape=chunk_shape, overlap=overlap)
+
+        done_iterator = ReadyNeighborIterator(block)
+        it = done_iterator.get()
+        processed = 0
+        completed = set()
+        for chunk in it:
+            completed.add(chunk.unit_index)
+            if all(
+                neighbor in completed for neighbor in done_iterator.get_all_neighbors(
+                    chunk.unit_index, block.num_chunks)) and chunk.unit_index not in completed:
+                it.send(chunk)
+            processed += 1
+
+        assert reduce(lambda x, y: x * y, block.num_chunks) == processed
